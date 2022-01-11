966fc1b0709980c69f89d1e415d698e685a4adbe
Apirak Panatkool <apirakb@gmail.com>
2022-01-11 07:28:07 +0700

    chapter 78

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {lat: null, errorMessage: ''};
+  state = {lat:null, errorMessage: ''};
 
+  componentDidMount(){
+    console.log('component did mount');
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat: position.coords.latitude});
-      },
-      (err) => {
-        this.setState({errorMessage: err.message});
-      }
-    );
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message}),
+    )
   }
 
-  render() {
+  // componentDidUpdate() {
+  //   console.log('component Did update');
+  // }
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
+    // return <Spinner />;
+    return <Spinner message="Please accpet location request" />;
+  };
 
-    return <div>Loading</div>;
-  }
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
+  };
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
